b870796a5c985781df1b6bedb4bf3457618227a7
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-11-17 11:36:55 -0500

    11/17

--- src/NumberFun.java
+++ src/NumberFun.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class NumberFun {
+public class NumberFun {
+    public NumberFun(){}
+
+    public void printNums(int value, int numRounds){
+        for (int i = 1; i<=numRounds; i++){
+            int f = -1;
+            while(f!=value){
+                f = (int)(Math.random()*10);
+                System.out.print(f);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Runner {
+public class Runner {
+    public static void main(String[]args){
+        NumberFun fun = new NumberFun();
+        System.out.println("New Round: ");
+        fun.printNums(4, 5);
+        System.out.println();
+
+        System.out.println("New Round: ");
+        fun.printNums(1, 10);
+        System.out.println();
+
+        System.out.println("New Round: ");
+        fun.printNums(6, 8);
+        System.out.println();
+
+        System.out.println("New Round: ");
+        fun.printNums(7, 3);
+    }
 }
